7ecc8781725eb1c75cfc574f7f9efb1339a5f8f8
Omar Al-Ithawi <i@omardo.com>
2019-06-18 16:34:00 +0300

    fixup

--- edx-platform/xblocks/xblocks.py
+++ edx-platform/xblocks/xblocks.py
@@ -10,21 +10,25 @@ from os import system, mkdir
 from os import path
 import time
 
+XBLOCKS_DIR = path.dirname(__file__)
 
 def xblock_configs():
-    with open('config.yaml', 'r') as config_file:
+    with open(path.join(XBLOCKS_DIR, 'config.yaml'), 'r') as config_file:
         config = safe_load(config_file)
 
-    for xblock in config['blocks']:
+    for xblock in config['xblocks']:
         yield xblock
 
 
 def pull_translations():
     for config in xblock_configs():
-        repo_dir = path.join('xblocks/repos', config['name'])
+        repos_dir = path.join(XBLOCKS_DIR, 'repos')
+        repo_dir = path.join(repos_dir, config['name'])
+
         branch_name = 'i18n-bot/{time}'.format(time=time.strftime('%Y-%m-%d-%H%M%S'))
         rmtree(repo_dir, True)
-        call(['git', 'clone', config['upstream_repo'], repo_dir])
+
+        call(['git', 'clone', config['upstream_repo'], config['name']], cwd=repos_dir)
         call(['git', 'remote', 'add', 'local', config['local_repo']], cwd=repo_dir)
         call(['git', 'fetch', '--all'], cwd=repo_dir)
         call(['git', 'checkout', '--branch', branch_name], cwd=repo_dir)
@@ -37,3 +41,7 @@ def pull_translations():
               # TODO: Change to something owned by the Open edX team
               '--author', 'Open edX i18n Bot <omar+openedx-i18n-bot@appsembler.com>',
         ], cwd=repo_dir)
+
+
+if __name__ == '__main__':
+    pull_translations()
